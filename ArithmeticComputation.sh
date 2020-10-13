3f8a6d51f4819d3a93f47a5ac0931a03d2dd9f1e
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-10-13 15:34:41 +0530

    Added : code for uc-9, to sort array in ascending order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -56,6 +56,25 @@ do
       fi
    done
 done
+echo ""
 echo "array after sorting in descending order is : "
 echo ${array[@]}
+
+
+#sort array in descending order
+for (( i=1; i<$counter; i++ ))
+do
+   for (( j=0; j<$i; j++ ))
+   do
+      if [[ ${array[j]} -gt ${array[i]} ]]
+      then
+         temp=${array[j]}
+         array[j]=${array[i]}
+         array[i]=$temp
+      fi
+   done
+done
 echo ""
+echo "array after sorting in ascending order is : "
+echo ${array[@]}
+
